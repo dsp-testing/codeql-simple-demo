135c55afa4490766448d2a799e83a613fdc70990
Niroshan Rajadurai <niroshan@github.com>
2020-04-29 12:15:30 +0100

    Fix stray {

--- main.c
+++ main.c
@@ -13,17 +13,16 @@ void branchingFactor()
 {
    int factor = atoi(getenv("BRANCHING_FACTOR"));
    /* GOOD: Prevent overflow by checking the input */
-   /*if (factor < 0 || factor > 1000) {
+   /******* FIX *******
+   if (factor < 0 || factor > 1000) {
        log("Factor out of range (%d)\n", factor);
        return -1;
    }
-   */
+   ******* FIX *******/
    // This line can allocate too little memory if factor
    // is very large.
    char **root_node = (char **) malloc(factor * sizeof(char *));
-   }
 }
-
 
 int main() {
    char aName[256] = {0};
